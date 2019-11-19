c3b7510f50e71090a571f6a448965bb5411543f5
leonardo.salazar <leonardo.salazar@CONSULTOR5.vasslatam.com>
2019-11-18 22:35:43 -0500

    IC-2019.2.3 <leonardo.salazar@CONSULTOR5 Overwrite remote https://github.com/nesvila90/app-repo-demo-vass/ to local

--- grupo-estudio/person/src/main/java/com/nexos/person/service/person/PersonService.java
+++
@@ -1,7 +0,0 @@
-package com.nexos.person.service.person;
-
-import com.nexos.person.domain.dto.generic.PersonDTO;
-import com.nexos.person.service.base.BaseService;
-
-public interface PersonService extends BaseService<PersonDTO> {
-}
